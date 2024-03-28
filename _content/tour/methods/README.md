cfd25f48a79d0d27559a57ea6530a504de0d9053
dancer1325 <alfredotic0809@gmail.com>
2024-03-28 20:06:18 +0100

    doc(tour.methods): add commentaries about empty interface

---
+++ _content/tour/methods/README.md
@@ -0,0 +1,11 @@
+# TODO:
+
+# `interface{}` -- empty interface - 
+* := `interface` / NO methods
+  * 👁️can hold values of any type👁️
+    * **Reason:** 🧠 every type implements zero methods🧠
+* uses
+  * code which handles values of unknown type
+* `go run empty-interface.go`
+
+# TODO:
--- _content/tour/methods/empty-interface.go
+++ _content/tour/methods/empty-interface.go
@@ -1,3 +1,4 @@
+//go:build OMIT
 // +build OMIT
 
 package main
@@ -8,6 +9,7 @@ func main() {
 	var i interface{}
 	describe(i)
 
+	// interface{}  can hold values of any type
 	i = 42
 	describe(i)
 
@@ -15,6 +17,9 @@ func main() {
 	describe(i)
 }
 
+// interface{}  because `Printf` can handle values of any type
 func describe(i interface{}) {
+	// %v		-- prints the -- operand's value
+	// %T		-- prints the -- operand's type
 	fmt.Printf("(%v, %T)\n", i, i)
 }
