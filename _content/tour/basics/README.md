71cdd5bbcbc8a6d1e73a133d319cffd4b3d772e4
dancer1325 <alfredotic0809@gmail.com>
2024-03-31 10:24:12 +0200

    doc(tour.basics.functions): add commentaries about returnedTypes

--- _content/tour/basics/README.md
+++ _content/tour/basics/README.md
@@ -24,7 +24,8 @@
   * `argumentVariable argumentType` ≠ C `argumentType argumentVariable`
   * if several consecutive arguments share the type → argumentType can be specified for the last one -- 
   `func functionName(argumentVariable1, argumentVariable2 argumentType, …) returnedType {…}`--
-* `go run functions.go` & `go run functions-continued.go` 
+  * `(returnedType1, returnedType2, …)` is feasible
+* `go run functions.go` & `go run functions-continued.go` & `go run multiple-results.go`
 
 # Variables
 * TODO:
--- _content/tour/basics/multiple-results.go
+++ _content/tour/basics/multiple-results.go
@@ -1,14 +1,29 @@
+//go:build OMIT
 // +build OMIT
 
 package main
 
 import "fmt"
 
+// (returnType1, returnType2)
 func swap(x, y string) (string, string) {
 	return y, x
 }
 
+// (returnType1, returnType2, returnType3)		-- Any number of returnedTypes are legal
+func swapWithMore(x, y string) (string, string, string) {
+	return y, x, "Hello"
+}
+
+// returnType1, returnType2		is NOT possible
+/*func swapWithoutParenthesis(x, y string) string, string {
+	return y, x
+}*/
+
 func main() {
 	a, b := swap("hello", "world")
 	fmt.Println(a, b)
+
+	c, d, e := swapWithMore("Alfred", "Rosi")
+	fmt.Println(c, d, e)
 }
