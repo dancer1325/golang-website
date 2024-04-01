0f5a13df0480c1a1b0bb718478dd0d1e4d1b3d96
dancer1325 <alfredotic0809@gmail.com>
2024-04-01 20:10:29 +0200

    doc(tour.typeInference): add commentaries

--- _content/tour/basics/type-inference.go
+++ _content/tour/basics/type-inference.go
@@ -1,3 +1,4 @@
+//go:build OMIT
 // +build OMIT
 
 package main
@@ -6,5 +7,15 @@ import "fmt"
 
 func main() {
 	v := 42 // change me!
+	w := v  // type inferred also
 	fmt.Printf("v is of type %T\n", v)
+	fmt.Printf("w is of type %T\n", w)
+
+	// Based on the precision
+	i := 42           // int
+	f := 3.142        // float64
+	g := 0.867 + 0.5i // complex128
+	fmt.Printf("i is of type %T\n", i)
+	fmt.Printf("f is of type %T\n", f)
+	fmt.Printf("g is of type %T\n", g)
 }
