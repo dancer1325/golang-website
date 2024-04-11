b46f257e64e0e27b69ee3bfecb6ed6863e3cb29a
dancer1325 <alfredotic0809@gmail.com>
2024-04-12 00:51:12 +0200

    doc(tour.concurrency.channel): add commentaries

--- _content/tour/concurrency/README.md
+++ _content/tour/concurrency/README.md
@@ -5,3 +5,14 @@
 * `argumentsToPass` are evaluated in the 👁️ current 👁️ goroutine
 * `function(argumentsToPass)` is executed in the 👁️ NEW goroutine 👁️
 * `go run goroutines.go`
+
+# `chan` — channel —
+* := typed conduit which
+  * receive valueS the channel -- via `channelVariable <- valueToSend`
+  * send valueS the channel -- via `assignToAVariable <- channelVariable`
+    * 👁️the goroutine with this statement waits for receiving it 👁️
+* ways to create them
+  * `make(chan TypesToSenViaTheChannel)`
+* uses
+  * sync goroutines without explicit locks OR condition variables -- run several times the next command, to check random different order printing the new goroutines-- 
+* `go run channels.go`
--- _content/tour/concurrency/channels.go
+++ _content/tour/concurrency/channels.go
@@ -1,3 +1,4 @@
+//go:build OMIT
 // +build OMIT
 
 package main
@@ -6,18 +7,32 @@ import "fmt"
 
 func sum(s []int, c chan int) {
 	sum := 0
+
+	// for first, second range sliceOrMap {…}
+	// first  _		because we want to skip it == NOT use it
 	for _, v := range s {
 		sum += v
 	}
+
+	fmt.Println("sum for s", s)
+
 	c <- sum // send sum to c
 }
 
 func main() {
+	// slice literal
 	s := []int{7, 2, 8, -9, 4, 0}
 
+	// initialize channel
 	c := make(chan int)
-	go sum(s[:len(s)/2], c)
-	go sum(s[len(s)/2:], c)
+
+	// goroutines using the same channel
+	go sum(s[:len(s)/2], c) // s[:len(s)/2] == s[:3] == [7,2,8]
+	go sum(s[len(s)/2:], c) // s[len(s)/2] == s[3] == [-9,4,0]
+
+	fmt.Println("main")
+
+	// This goroutine waits receiving the values from the other goroutines through the channel
 	x, y := <-c, <-c // receive from c
 
 	fmt.Println(x, y, x+y)
